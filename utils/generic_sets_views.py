03ee9f6efbef2419bf80c215824a312d4d6998dd
Jefferson de Barros Oliveira <102183069+JeffOliveiraDev@users.noreply.github.com>
2023-04-20 14:11:34 -0300

    feat: tarefa_3

--- utils/generic_sets_views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.views import Request, Response
-
-from utils.base_views import CreateBaseView
-
-
-# class CreateGenericView(CreateBaseView, GenericBaseView):
-#     def post(self, request: Request) -> Response:
-#         return super().create(request)
-    
