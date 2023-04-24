18d33f7d56a07293cda486313a24e9b4af403c6e
Lucas Mires <lucasmires10@gmail.com>
2023-04-24 11:30:06 -0300

    tarefa 1 finalizada

--- albums/urls.py
+++ albums/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-
-from . import views
-from songs import views as song_views
+from albums.views import AlbumView
 
 urlpatterns = [
-    path("albums/", views.AlbumView.as_view()),
-    path("albums/<int:pk>/songs/", song_views.SongView.as_view()),
+    path("albums/", AlbumView.as_view())
 ]
---
+++ songs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from songs.views import SongView
+
+urlpatterns = [
+    path("albums/<int:pk>/songs/", SongView.as_view())
+]
